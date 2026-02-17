b954ba1159899d54aa67a0d1b69a83270a59cb92
Mario Popescu <mario123@gmail.com>
2026-02-17 06:12:56 +0200

    fix render port

--- main.py
+++ main.py
@@ -43,5 +43,8 @@ def index():
 
     return render_template("index.html", provocare=provocare, mod=mod)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
